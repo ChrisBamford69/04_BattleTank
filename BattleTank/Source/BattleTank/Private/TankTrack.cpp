0ba703dc827f4e00ae5578d340c530b3f8960d8a
Unknown <bamford.chris@gmail.com>
2018-08-30 08:23:50 +0100

    Resync with remote
    Ensure all changes pushed to remote for user account switch

---
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankTrack.h"
+
+void UTankTrack::SetThrottle(float Throttle)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Throttle set on %s to %f"), *GetName(), Throttle);
+	
+	auto ForceApplied = GetForwardVector() * Throttle * MaximumDrivingForce;
+	auto ForceLocation = GetComponentLocation();
+	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
+}
---
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/StaticMeshComponent.h"
+#include "TankTrack.generated.h"
+
+/**
+ * 
+ */
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class BATTLETANK_API UTankTrack : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+
+private:
+	UFUNCTION(BlueprintCallable, Category = Input)
+	void SetThrottle(float Throttle);
+
+	// Maximum driving force in Newtons
+	UPROPERTY(EditDefaultsOnly, Category = Input)
+	float MaximumDrivingForce = 400000.0f;
+};
